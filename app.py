a7a368662934c86a97e5665702e283888b030843
IIPeteII <pera17ab@student.cbs.dk>
2022-11-25 21:08:48 +0100

    ok trying again

--- app.py
+++ app.py
@@ -11,19 +11,19 @@ import io #Buffers for DF's
 import http.client #API
 import os #operating system functions
 #from selenium import webdriver #google search
-import chromedriver_autoinstaller #chrome driver to open a browser
-from PIL import image #open pictures
-from pathlib import Path #path function
-from scipy.io import loadmat #load .mat files
-import datetime #dates and time stuff
+#import chromedriver_autoinstaller #chrome driver to open a browser
+#from PIL import image #open pictures
+#from pathlib import Path #path function
+#from scipy.io import loadmat #load .mat files
+#import datetime #dates and time stuff
 
 #ML/Computer Vision
-import cv2 #computer vision
-import tensorflow as tf #tensorflow
-from keras.preprocessing.image import ImageDataGenerator #generate
-from keras.models import Sequential #sequential model
-from keras.layers import Dense, Flatten, MaxPooling2D, Dropout, Conv2D #model functions
-from keras import optimizers #model optimizers such as Adam and learning rates
+#import cv2 #computer vision
+#import tensorflow as tf #tensorflow
+#from keras.preprocessing.image import ImageDataGenerator #generate
+#from keras.models import Sequential #sequential model
+#from keras.layers import Dense, Flatten, MaxPooling2D, Dropout, Conv2D #model functions
+#from keras import optimizers #model optimizers such as Adam and learning rates
 
 #Deployment
 import streamlit as st #app deployment
