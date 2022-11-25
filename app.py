441b143bd21f2ebfab493460f6a4b5ed7dc0bd13
IIPeteII <pera17ab@student.cbs.dk>
2022-11-25 21:23:52 +0100

    image work please

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ import streamlit as st #app deployment
 #Introduction
 
 st.title('Car number plate detection')
-app_image = Image.open('C:\Users\Peter\Documents\GitHub\car-license-plate-recognition-clean\app-images\app_lambo_front.jpg')
+app_image = Image.open(r'C:\Users\Peter\Documents\GitHub\car-license-plate-recognition-clean\app-images\app_lambo_front.jpg')
 st.image(app_image, caption='Credits to https://unsplash.com/@reinhartjulian for the picture')
 st.header('Detecting license plates and returning an image of the car')
 
