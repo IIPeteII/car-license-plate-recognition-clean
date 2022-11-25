e8411aa1dad780c9930bba1a143233ee78690f89
IIPeteII <pera17ab@student.cbs.dk>
2022-11-25 21:31:00 +0100

    comon picture, work

--- app.py
+++ app.py
@@ -6,8 +6,10 @@
 import pandas as pd #DF work
 import numpy as np #Functions
 #import matplotlib.pyplot as plt #Visualizations
+import requests
 import altair as alt #Visualizations
 import io #Buffers for DF's
+from io import BytesIO
 import http.client #API
 import os #operating system functions
 #from selenium import webdriver #google search
@@ -31,7 +33,8 @@ import streamlit as st #app deployment
 #Introduction
 
 st.title('Car number plate detection')
-app_image = Image.open('https://github.com/IIPeteII/car-license-plate-recognition-clean/blob/b400547cb94976b2921b31bf974208b5edd8ea74/app-images/app_lambo_front.jpg')
+image_url = requests.get('https://raw.githubusercontent.com/IIPeteII/car-license-plate-recognition-clean/main/app-images/app_lambo_front.jpg')
+app_image = Image.open(BytesIO(image_url.content))
 st.image(app_image, caption='Credits to https://unsplash.com/@reinhartjulian for the picture')
 st.header('Detecting license plates and returning an image of the car')
 
@@ -52,5 +55,3 @@ st.subheader('Picture of car from dataset')
 #------------ Google search
 st.subheader('Link to google of car')
 #st.button('press here to see the car')
-
-st.write()
