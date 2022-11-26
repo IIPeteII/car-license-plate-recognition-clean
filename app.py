ee1ccf5f36ed78352949af59beef699d38dde465
IIPeteII <pera17ab@student.cbs.dk>
2022-11-26 19:48:15 +0100

    tying uploaded file to code

--- app.py
+++ app.py
@@ -68,20 +68,20 @@ if uploaded_file is not None:
     st.write(dataframe)
 
 #take a picture
-img_file_buffer = st.camera_input("Take a picture")
+#img_file_buffer = st.camera_input("Take a picture")
 
-if img_file_buffer is not None:
+#if img_file_buffer is not None:
     # To read image file buffer with OpenCV:
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    #bytes_data = img_file_buffer.getvalue()
+    #cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
     # Check the type of cv2_img:
     # Should output: <class 'numpy.ndarray'>
-    st.write(type(cv2_img))
+    #st.write(type(cv2_img))
 
     # Check the shape of cv2_img:
     # Should output shape: (height, width, channels)
-    st.write(cv2_img.shape)
+    #st.write(cv2_img.shape)
 
 #------------ License plate detection model
 st.subheader('License plate detection model')
